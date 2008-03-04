23895ed45cf28cd7041e2795e0c8480dab787a72
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2008-03-04 21:01:42 +0000

    skel
    git-svn-id: https://svn.socialplanning.org/svn/oc-tt/trunk@14003 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='oc-tt',
+      version=version,
+      description="opencore tasktracker client package",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['opencore'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [opencore.versions]
+      oc-tt = opencore.tasktracker
+      """,
+      )
